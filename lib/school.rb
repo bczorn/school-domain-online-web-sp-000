c0bf26508c4c0f55189a407f3e164e7a19ac7a3d
Becky Zorn <bczorn@gmail.com>
2019-09-24 22:20:21 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+
+  attr_accessor :school, :student
+  
+  
+  def initialize(school)
+    @school = school
+  end
+  
+  def roster
+    
+  end
+  
+  def add_student(student, grade)
+    @roster = {grade => [student]}
+    
+  end
+  
+  def grade(student)
+    
+    
+  end
+  
+end
